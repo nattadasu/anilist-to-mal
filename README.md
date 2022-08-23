c39233df067454211c6a2fe97138fbc3230eed1c
Sultan Iskandar Maulana <sultan.ridita@gmail.com>
2022-08-23 10:27:28 +0700

    Update script

--- README.md
+++ README.md
@@ -1,31 +1,37 @@
 # Anilist to MAL
 Anilist to MAL is a simple python tool that lets you export your anime list from [AniList](https://anilist.co) to [MyAnimeList](https://myanimelist.net).
 
-**PLEASE NOTE** i don't really maintain this anymore! i haven't updated it in ages, so i don't even know if it still works. feel free to make forks and pull requests, and i'll get to issues as they come, but i'm not really actively checking for issues or anything.
+This fork is maintained version of upstream, as the repo is being used for [nattadasu/animeManga-autoBackup](https://github.com/nattadasu/anime)
 
 ## Changes of this fork
 
-* This version allows to export a **specific list** (with -l option) created in your Anilist profile in order to export only the contents of this list and not full list. (Only implemented for XML format)
+* This version added more metadata to the XML file. Including, but not limited to:
+  * Started date
+  * Finished date
+  * Anime/manga title (by default commented to avoid conflict with MAL romanization schema)
+  * Comment and Tags (Note in AniList)
+* Added `-h`/`--help` for show help and valid arguments list.
+* Added long arguments for readability.
+* `-f`/`--format` is unsupported, only XML is allowed for output.
 
 ## Usage
 This app requires [Python 3](https://www.python.org/downloads/) and [Python Requests](http://docs.python-requests.org/en/master/).
 
 Install Python Requests with `pip3 install requests`.
 
-Once both are installed, run the app with `python3 main.py` and follow the instructions!
+Once both are installed, run the app with `python3 main.py` and follow the instructions, or pass some [arguments](#arguments) into command.
 
 ### Arguments
 In addition to the step-by-step method, you can pass some arguments to the script.
 
-- `-f`: set filetype (default `xml`)
-  - options: `xml`, `txt`
-  - txt will generate a markdown-compatible list format text file
-- `-u`: set username (default `''`)
-- `-t`: set list type (default `''`)
+- `-u`, `--username <name>`: set username, if not filled, script will prompt
+- `-t`, `--type <media>`: set list type, if not filled, script will prompt
   - options: `anime`, `manga`
-- `-n`: takes one argument, set the file name to be exported to
+- `-n <name>`, `-o <name>`, `--set-name <name>`, `--out-file <name>`: takes one argument, set the file name to be exported to
 
-- `-s`: takes zero arguments, run in silent mode (no non-essential prints)
-- `-p`: takes zero arguments, show progress while running
+- `-s`, `--silent`: takes zero arguments, run in silent mode (no non-essential prints)
+- `-p`, `--show-progress`: takes zero arguments, show progress while running
 
-- `-l`: select a specific list to export of your profile (default `''`)
+- `-l`, `--custom-list`: select a specific list to export of your profile (default `''`)
+
+- `-h`, `--help`: shows help page. If argument added, it will end the script.
--- main.py
+++ main.py
@@ -1,40 +1,12 @@
 #!/usr/bin/env python3
 
-import requests, pprint, sys, getopt
-
-# Test query for https://anilist.co/graphiql
-# {
-#   MediaListCollection(userName: "nathan", type: ANIME) {
-#     lists {
-#       entries {
-#         id
-#         status
-#         score(format: POINT_10)
-#         progress
-#         notes
-#         repeat
-#         media {
-#           chapters
-#           volumes
-#           idMal
-#           episodes
-#           title { romaji }
-#         }
-#       }
-#       name
-#       isCustomList
-#       isSplitCompletedList
-#       status
-#     }
-#   }
-# }
+import requests, pprint, sys, getopt, datetime
 
 # What kind of output do we want
 outputFile = 'xml'
 outputTxt = ''''''
 silent = False
 showProgress = False
-name = 'anilist-export'
 listName = ''
 complete = True
 # Define our query variables and values that will be used in the query request
@@ -42,52 +14,65 @@ variables = {
   'username': '',
   'type': 'ANIME'
 }
+name = 'anilist-export_' + variables['type']
 
-url = 'https://graphql.anilist.co'
-
-def main(argv):
-#   print('''
-#                   ┏━━━━━━━━━━━━━━━━━━┓
-#                   ┃  AniList to MAL  ┃
-#                   ┗━━━━━━━━━━━━━━━━━━┛
-
-# An export tool for Anilist to import to MyAnimeList.
-# Enter your username, and this will generate a MyAnimeList
-# compatible XML file to import at https://myanimelist.net/import.php
-
-# Made by Nathan Wentworth (https://nathanwentworth.co)
-
-# Report any problems to me on twitter! https://twitter.com/nathanwentworth
-# ''')
-
-  global outputFile, silent, name, showProgress, listName, complete
-
-  for index, arg in enumerate(argv):
-    if arg == "-f" or arg == "--file":
-      outputFile = argv[index + 1]
-    elif arg == "-u":
-      variables['username'] = argv[index + 1]
-    elif arg == "-t":
-      variables['type'] = argv[index + 1].upper()
-    elif arg == "-s":
-      silent = True
-    elif arg == "-n":
-      name = argv[index + 1]
-    elif arg == "-p":
-      showProgress = True
-    elif arg == "-l":
-      listName = argv[index + 1]
-
-  if not silent:
-    print('''
+# Configure Banner
+banner = '''
   ┏━━ AniList to MAL ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
   ┃ An export tool for Anilist to import to MyAnimeList.          ┃
   ┃ Enter your username, and this will generate an XML file       ┃
   ┃ to import here: https://myanimelist.net/import.php            ┃
   ┃ Made by Nathan Wentworth (https://nathanwentworth.co)         ┃
-  ┃ Report any problems here: https://twitter.com/nathanwentworth ┃
+  ┠───────────────────────────────────────────────────────────────┨
+  ┃ Forked and maintained by Natsu Tadama (https://nttds.my.id)   ┃ 
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-''')
+'''
+
+url = 'https://graphql.anilist.co'
+
+def main(argv):
+  global outputFile, silent, name, showProgress, listName, complete
+
+  for index, arg in enumerate(argv):
+    if arg == "-u" or arg == "--username":
+      variables['username'] = argv[index + 1]
+    elif arg == "-t" or arg == "--type":
+      variables['type'] = argv[index + 1].upper()
+    elif arg == "-s" or arg == "--silent":
+      silent = True
+    elif arg == "-n" or arg == "-o" or arg == "--set-name" or arg == "--out-file":
+      name = argv[index + 1]
+    elif arg == "-p" or arg == "--show-progress":
+      showProgress = True
+    elif arg == "-l" or arg == "--custom-list":
+      listName = argv[index + 1]
+    # Return help
+    elif arg == "-h" or arg == "--help":
+      print(banner + '''
+Usage: python main.py [options...]
+-u, --username <string>
+    AniList Username
+-t, --type <media>
+    Set media type to export
+    Default is ''
+    Options: anime, manga
+-n, --set-name <name>
+-o, --out-file <name>
+    Set file name
+    Default is anilist-export<media>.<format>
+-s, --silent
+    Disable banner
+-p, --show-progress
+    Show progress bar
+-l, --custom-list
+    Select custom list
+-h, --help
+    Show this help menu
+      ''')
+      return
+
+  if not silent:
+    print(banner)
 
   if (listName != ''):
     complete = False
@@ -140,6 +125,18 @@ def getAnilistData():
             episodes
             title { romaji }
           }
+          startedAt{
+            year
+            month
+            day
+          }
+          completedAt{
+            year
+            month
+            day
+          }
+          updatedAt
+          createdAt
         }
         name
         isCustomList
@@ -163,47 +160,7 @@ def getAnilistData():
   if len(statusLists) < 1:
     print('No items found in this list!\nDid you enter the wrong username?')
     return;
-  if silent == False:
-    convertAnilistDataToTxt(statusLists)
-  if outputFile == 'xml':
-    convertAnilistDataToXML(statusLists)
-  elif outputFile == 'txt':
-    if silent == True:
-      convertAnilistDataToTxt(statusLists)
-    writeToFile(outputTxt)
-
-def convertAnilistDataToTxt(data):
-  listOrder = ['current', 'completed', 'paused', 'planning', 'dropped']
-
-  global outputTxt
-  if variables['type'] == 'ANIME':
-    outputTxt = "# Anime List\n"
-  else:
-    outputTxt = "# Manga List\n"
-
-  for listStatus in listOrder:
-    if listStatus in data:
-      outputTxt += '\n## ' + listStatus.capitalize() + '\n'
-      data[listStatus].sort(key=lambda show: show['score'], reverse=True)
-      for item in data[listStatus]:
-        outputTxt += '- '
-
-        progress = '{0: >3}'.format(str(item['progress'])) + '/'
-        progress += '{0: <3}'.format(str(item['media']['episodes']) if item['media']['episodes'] is not None else '??')
-        if showProgress and listStatus == 'current' or listStatus == 'paused' or listStatus == 'dropped':
-          outputTxt += progress + ' '
-        else:
-          outputTxt += '        ' # 8 characters, same as progress block
-
-        title = str(item['media']['title']['romaji'])
-        outputTxt += title
-
-        if item['score'] > 0:
-          outputTxt += '\tScore: ' + str(item['score']) + '/10'
-        outputTxt += '\n'
-
-  if not silent:
-    print(outputTxt)
+  convertAnilistDataToXML(statusLists)
 
 def convertAnilistDataToXML(data):
   output = ''''''
@@ -241,32 +198,53 @@ def convertAnilistDataToXML(data):
           s = "Completed"
           user_total_completed += 1
 
-        animeItem = ''
-        animeItem += '        <anime>\n'
-        animeItem += '          <series_animedb_id>' + str(item['media']['idMal']) + '</series_animedb_id>\n'
-        animeItem += '          <series_episodes>' + str(item['media']['episodes']) + '</series_episodes>\n'
-        animeItem += '          <my_watched_episodes>' + str(item['progress']) + '</my_watched_episodes>\n'
-        animeItem += '          <my_score>' + str(item['score']) + '</my_score>\n'
-        animeItem += '          <my_status>' + s + '</my_status>\n'
-        animeItem += '          <my_times_watched>' + str(item['repeat']) + '</my_times_watched>\n'
+        # Format time
+        startedYear = str(item['startedAt']['year'])
+        completedYear = str(item['completedAt']['year'])
+        if startedYear == "None":
+          startedDate = "0000-00-00"
+        else:
+          startedDate = datetime.datetime(int(startedYear), int(item['startedAt']['month']), int(item['startedAt']['day'])).strftime('%Y-%m-%d')
+        if completedYear == "None":
+          completedDate = "0000-00-00"  
+        else:
+          completedDate = datetime.datetime(int(completedYear), int(item['completedAt']['month']), int(item['completedAt']['day'])).strftime('%Y-%m-%d')
 
-        animeItem += '          <update_on_import>1</update_on_import>\n'
-        animeItem += '        </anime>\n\n'
+        notes = item['notes']
+        if notes == "None" or notes is None:
+          notes = ""
+
+        animeItem = ''
+        animeItem += '    <anime>\n'
+        animeItem += '        <series_animedb_id>' + str(item['media']['idMal']) + '</series_animedb_id>\n'
+        animeItem += '        <!-- <series_title>' + str(item['media']['title']['romaji']) + '</series_title> -->\n'
+        animeItem += '        <series_episodes>' + str(item['media']['episodes']) + '</series_episodes>\n'
+        animeItem += '        <my_watched_episodes>' + str(item['progress']) + '</my_watched_episodes>\n'
+        animeItem += '        <my_start_date>' + startedDate + '</my_finish_date>\n'
+        animeItem += '        <my_finish_date>' + completedDate + '</my_finish_date>\n'
+        animeItem += '        <my_score>' + str(item['score']) + '</my_score>\n'
+        animeItem += '        <my_status>' + s + '</my_status>\n'
+        animeItem += '        <my_comments><![CDATA[' + str(notes) + ']]></my_comments>\n'
+        animeItem += '        <my_tags><![CDATA[' + str(notes) + ']]></my_tags>\n'
+        animeItem += '        <my_times_watched>' + str(item['repeat']) + '</my_times_watched>\n'
+        animeItem += '        <update_on_import>1</update_on_import>\n'
+        animeItem += '    </anime>\n\n'
 
         output += animeItem
         user_total_anime += 1
 
 
   outputStart = '''<?xml version="1.0" encoding="UTF-8" ?>
-    <!--
-     Created by XML Export feature at MyAnimeList.net
-     Programmed by Xinil
-     Last updated 5/27/2008
-    -->
+<!--
+  Created by XML Export feature at MyAnimeList.net
+  Programmed by Xinil
+  Last updated 5/27/2008
+  Exported at ''' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S %z') + '''
+-->
 
-    <myanimelist>
+<myanimelist>
 
-      <myinfo>
+    <myinfo>
         <user_id>123456</user_id>
         <user_name>''' + variables['username'] + '''</user_name>
         <user_export_type>1</user_export_type>
@@ -276,10 +254,10 @@ def convertAnilistDataToXML(data):
         <user_total_onhold>''' + str(user_total_onhold) + '''</user_total_onhold>
         <user_total_dropped>''' + str(user_total_dropped) + '''</user_total_dropped>
         <user_total_plantowatch>''' + str(user_total_plantowatch) + '''</user_total_plantowatch>
-      </myinfo>
+    </myinfo>
 
 '''
-  output = outputStart + output + '      </myanimelist>'
+  output = outputStart + output + '</myanimelist>'
 
   writeToFile(output)
 
@@ -289,7 +267,7 @@ def convertAnilistDataToXML(data):
 
 
 def writeToFile(output):
-  f = open(name + variables['type'] + '.' + outputFile, 'w')
+  f = open(name + '.' + outputFile, 'w', encoding="utf-8")
   f.write(output)
   f.close()
 
